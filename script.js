9b0f6d19b68f1d2925146dc067b435b977f6cd29
Emma Banfi <e.banfi@italcert.local>
2024-10-07 11:11:01 +0200

    changes
    changes

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('submit').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password').value;
+    const correctPassword = '123'; 
+
+    if (passwordInput === correctPassword) {
+        window.location.href = 'welcome.html'; 
+    } else {
+        laughSound.play();
+    }
+}); 
+
